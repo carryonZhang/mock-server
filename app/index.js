6a5a5a7c5300487ec074c80526230c15682a7de9
carryonZhang <xunlie2008@126.com>
2017-06-21 12:42:20 +0800

    项目更新

--- app/index.js
+++ app/index.js
@@ -2,7 +2,6 @@
  * Created by shaolong on 2017/6/14.
  */
 const Koa = require('koa');
-const serve = require('koa-static');
 const path = require('path');
 const Router = require('koa-router');
 const fs = require('fs');
@@ -26,12 +25,46 @@ let readFromFile = () => {  //读文件函数
 	})
 }
 
-
-router.get('/mock', async (ctx, next) => {
+router.get('/dota2', async (ctx, next) => {
 		let data = await readFromFile();
+		ctx.type = 'application/json';
 		ctx.body = JSON.stringify({status: 'success', data: data.toString()})
 	}
 )
+
+router.get('/dota2/news_detail', async (ctx, next) => {
+	let news_id = ctx.request.query.news_id;
+
+	if(Number.isNaN(parseInt(news_id))) {
+		handleError(ctx)
+	}else {
+
+		news_id = parseInt(news_id)
+	}
+	let data = await readFromFile();
+	data = data ? JSON.parse(data) : handleError(ctx);
+	let news = data.news;
+
+	const send = (ns) => {
+		ns = JSON.stringify(ns);
+		ctx.type = 'application/json';
+		ctx.body = JSON.stringify({status: 'success', detail: ns})
+	}
+
+	news.forEach((ns) => {
+		if (ns.id === news_id) {
+			send(ns);
+			return;
+		}
+	});
+
+})
+
+const handleError = (ctx) => {
+	console.log('handleError');
+	ctx.body = {status: 'fail', data: '请传新闻id'};
+	return;
+}
 
 app.use(router.routes())
 	.use(router.allowedMethods());
